18da3f222b12bfe97fb811c43c7c61c6b0a82e8c
CristhianNina <cristhian.luis.nina@gmail.com>
2019-12-11 17:17:28 -0400

    Insertando api de geolocalizacion

---
+++ script.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+    var coord = {lat:-22.0111946,lng:-63.6764021};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
